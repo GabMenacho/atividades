f2ebd8a208afee6b5be6b4bc83fbc5a62d5c247a
GabMenacho <menacho.gabriela@gmail.com>
2021-10-25 21:58:41 -0300

    Aula FOR IF 25-10-2021

---
+++ questao5_25_10_2021.js
@@ -0,0 +1,11 @@
+console.log ('\n')
+
+for ( let alunos = 0; alunos <=17; alunos ++) {
+    if (alunos == 0) {
+        console.log(alunos + " é ZER0")
+    } else if ( alunos % 2 == 0 ) {
+        console.log(alunos + ' é par')
+    } else {
+        console.log(alunos + ' é ímpar')
+    };
+};
